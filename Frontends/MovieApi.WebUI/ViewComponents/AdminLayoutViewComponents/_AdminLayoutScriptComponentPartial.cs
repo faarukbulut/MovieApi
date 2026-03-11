ed171fdc1b13438a8be6feb7b90bdb67b8ecb2c9
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2026-03-12 00:51:10 +0300

    admin layout işlemleri

---
+++ Frontends/MovieApi.WebUI/ViewComponents/AdminLayoutViewComponents/_AdminLayoutScriptComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.ViewComponents.AdminLayoutViewComponents
+{
+    public class _AdminLayoutScriptComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
