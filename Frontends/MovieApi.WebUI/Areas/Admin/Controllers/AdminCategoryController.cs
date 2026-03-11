ab5ae1a29053b28229c16762a3d2f4dccfcb2eec
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2026-03-12 01:12:27 +0300

    admin kategori listelenmesi

---
+++ Frontends/MovieApi.WebUI/Areas/Admin/Controllers/AdminCategoryController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Dto.Dtos.AdminCategoryDtos;
+using Newtonsoft.Json;
+
+namespace MovieApi.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminCategoryController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminCategoryController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> CategoryList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44380/api/Categories");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<AdminResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+    }
+}
