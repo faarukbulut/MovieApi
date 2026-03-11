e879c1030ec8c00e91994ad4e9d072a82513b2e5
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2026-03-11 23:49:57 +0300

    identity

---
+++ Infrastructure/MovieApi.Persistence/Identity/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MovieApi.Persistence.Identity
+{
+    public class AppUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
