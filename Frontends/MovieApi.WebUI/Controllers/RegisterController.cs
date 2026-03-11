96bf1f1417f667c9993beaf7bfcebc2483586ba7
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2026-03-12 00:16:29 +0300

    Kullanıcı kayıt işlemleri

---
+++ Frontends/MovieApi.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Dto.Dtos.UserRegisterDtos;
+using Newtonsoft.Json;
+
+namespace MovieApi.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public RegisterController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(CreateUserRegisterDto createUserRegisterDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createUserRegisterDto);
+            StringContent stringContent = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:44380/api/Registers", stringContent);
+
+            if(response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("SignIn", "Login");
+            }
+
+            ViewBag.error = "Kayıt başarısız oldu. Lütfen tekrar deneyiniz.";
+            return View();
+        }
+    }
+}
--- Presentation/MovieApi.WebApi/Program.cs
+++ Presentation/MovieApi.WebApi/Program.cs
@@ -1,8 +1,10 @@
-using System.Reflection;
+using Microsoft.AspNetCore.Identity;
 using MovieApi.Application.Features.CQRSDesignPattern.Handlers.CategoryHandlers;
 using MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers;
+using MovieApi.Application.Features.CQRSDesignPattern.Handlers.UserRegisterHandlers;
 using MovieApi.Application.Features.MediatorDesignPattern.Handlers.TagHandlers;
 using MovieApi.Persistence.Context;
+using MovieApi.Persistence.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,8 +22,11 @@ builder.Services.AddScoped<CreateMovieCommandHandler>();
 builder.Services.AddScoped<RemoveMovieCommandHandler>();
 builder.Services.AddScoped<UpdateMovieCommandHandler>();
 
+builder.Services.AddScoped<CreateUserRegisterCommandHandler>();
+
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetTagQueryHandler).Assembly));
 
+builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<MovieContext>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
