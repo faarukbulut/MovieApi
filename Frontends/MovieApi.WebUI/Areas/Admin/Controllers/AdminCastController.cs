e753b32819742f16a285b6db154c1b7efa9b6d6f
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2026-03-12 17:19:51 +0300

    cast list view

---
+++ Frontends/MovieApi.WebUI/Areas/Admin/Controllers/AdminCastController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminCastController : Controller
+    {
+        public IActionResult CastList()
+        {
+            return View();
+        }
+    }
+}
